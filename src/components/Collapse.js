5590f3e16a59d813a1341999b4869bad9df3a8de
Erdij <yildirimsukruerdi@gmail.com>
2022-06-16 02:28:54 +0300

    props_finished

---
+++ src/components/Collapse.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Collapse = (props) => {
+  // console.log(props.children);
+  // console.log(props);
+  return (
+    <div>
+      <a
+        className="btn btn-primary w-100"
+        data-toggle="collapse"
+        href={"#".concat(props.href)}
+        role="button"
+        aria-expanded="false"
+        aria-controls="collapseExample"
+      >
+        Link with href
+      </a>
+
+      <div className="collapse show " id={props.href}>
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+export default Collapse;
---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./components/Card";
+import Collapse from "./components/Collapse";
+
+const App = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="card-group">
+          <div className="col">
+            <Collapse href="collapseExample1">
+              <Card
+                cardTitle="Card Title 1 "
+                cardText="Lorem Ipsum Text 1"
+                updatedTime="Last Updated 1 min ago"
+                image="https://picsum.photos/id/1/200/300"
+              />
+            </Collapse>
+          </div>
+          <div className="col">
+            <Collapse href="collapseExample2">
+              <Card
+                cardTitle="Card Title 2 "
+                cardText="Lorem Ipsum Text 2"
+                updatedTime="Last Updated 2 min ago"
+                image="https://picsum.photos/id/2/200/300"
+              />
+            </Collapse>
+          </div>
+          <div className="col">
+            <Collapse href="collapseExample3">
+              <Card
+                cardTitle="Card Title 3 "
+                cardText="Lorem Ipsum Text 3"
+                updatedTime="Last Updated 3 min ago"
+                image="https://picsum.photos/id/3/200/300"
+              />
+            </Collapse>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("ses"));
